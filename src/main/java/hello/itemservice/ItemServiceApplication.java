830b965fcc5bd74c3b2b37ccd1bdf228cb33c267
김민수 <kimms2@lionplus.co.kr>
2023-10-10 16:32:55 +0900

    231010

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,12 +2,26 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
+
 	}
+
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");  //messages.properties와 errors.properties를 읽어옴.
+//		//경로는 /resources/messages.properties
+//		messageSource.setDefaultEncoding("utf-8");
+//		// 인코딩은 보통 utf-8
+//		return messageSource;
+//	}
 
 }
